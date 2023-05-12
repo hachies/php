138f65f663e42af43387b9c2bea791706cb28aa4
hachies <133209891+hachies@users.noreply.github.com>
2023-05-12 19:18:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello world";
+    
+    $variable1 = 12;
+    $variable2 = 10;
+
+    $resultaat = $variable1 * $variable2;
+
+    echo "the resultaat is:" . $resultaat;
+
+    $mijnNaam = "Carl";
+    $jouwNaam = "Camel";
+    echo $mijnNaam . " zit bij " . $jouwNaam ." in de klas! ";
+    ?>
+</body>
+</html>
